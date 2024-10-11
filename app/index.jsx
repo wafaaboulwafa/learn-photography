568364ff703bcec82a50c2e645002b9dfba5d139
Wafa Aboulwafa <67149147+wa250031@users.noreply.github.com>
2024-10-11 17:35:17 +0300

    Topics

--- app/index.jsx
+++ app/index.jsx
@@ -1,159 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { Link } from "expo-router";
+import topics from "../constants/topics";
+
+const Topic = ({ rec }) => {
+  return (
+    <Link
+      href={{
+        pathname: rec.path,
+      }}
+      style={styles.topicContainer}
+    >
+      <Text style={styles.topicText}>{rec.title}</Text>
+    </Link>
+  );
+};
 
 const Index = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>فهرس المحتويات</Text>
-      <Link
-        href={{
-          pathname: "AuthorIntro",
-        }}
-      >
-        مقدمة الكاتب
-      </Link>
-      <Link
-        href={{
-          pathname: "IntroToPhotography",
-        }}
-      >
-        مقدمة في التصوير
-      </Link>
-      <Link
-        href={{
-          pathname: "Composition",
-        }}
-      >
-        تكوين الصورة
-      </Link>
-      <Link
-        href={{
-          pathname: "LightProperties",
-        }}
-      >
-        خصائص الضوء و أنواعه
-      </Link>
-      <Link
-        href={{
-          pathname: "ExposureTriangle",
-        }}
-      >
-        مثلث التعريض الضوئي
-      </Link>
-      <Link
-        href={{
-          pathname: "WhiteBalance",
-        }}
-      >
-        موازنة اللون الأبيض
-      </Link>
-      <Link
-        href={{
-          pathname: "LightMetering",
-        }}
-      >
-        مقياس الضوء
-      </Link>
-      <Link
-        href={{
-          pathname: "CameraFocus",
-        }}
-      >
-        التركيز و خصائصه
-      </Link>
-      <Link
-        href={{
-          pathname: "CameraFlash",
-        }}
-      >
-        فلاش الكاميرا
-      </Link>
-      <Link
-        href={{
-          pathname: "CameraLenses",
-        }}
-      >
-        عدسات الكاميرا
-      </Link>
-      <Link
-        href={{
-          pathname: "LightRoom",
-        }}
-      >
-        معالجة الصور ببرنامج لايت روم
-      </Link>
-      <Link
-        href={{
-          pathname: "PhotographySpecializations",
-        }}
-      >
-        محاور التصوير الفوتوغرافي
-      </Link>
-      <Link
-        href={{
-          pathname: "LandscapePhotography",
-        }}
-      >
-        تصوير الطبيعة
-      </Link>
-      <Link
-        href={{
-          pathname: "NightPhotography",
-        }}
-      >
-        التصوير الليلي
-      </Link>
-
-      <Link
-        href={{
-          pathname: "ArchitecturePhotography",
-        }}
-      >
-        التصوير المعماري
-      </Link>
-      <Link
-        href={{
-          pathname: "PortraitPhotography",
-        }}
-      >
-        تصوير البورتريت
-      </Link>
-      <Link
-        href={{
-          pathname: "PhotoJournalist",
-        }}
-      >
-        التصوير الصحفي
-      </Link>
-      <Link
-        href={{
-          pathname: "SportsPhotography",
-        }}
-      >
-        تصوير الرياضة
-      </Link>
-      <Link
-        href={{
-          pathname: "AnimalsPhotography",
-        }}
-      >
-        تصوير الحيوانات
-      </Link>
-      <Link
-        href={{
-          pathname: "FlowersPhotography",
-        }}
-      >
-        تصوير الزهور
-      </Link>
-      <Link
-        href={{
-          pathname: "FinalWords",
-        }}
-      >
-        كلمة أخيرة
-      </Link>
+      <Text style={styles.header}>مواضيع الكتاب</Text>
+      <ScrollView style={styles.scrollContainer}>
+        {topics.map((r) => (
+          <Topic rec={r} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
@@ -162,6 +33,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    textAlign: "center",
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  topicContainer: {
+    flex: 1,
+    margin: 10,
+  },
+  topicText: {
+    fontSize: 16,
   },
 });
 
---
+++ constants/topics.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    path: "AuthorIntro",
+    title: "مقدمة الكاتب",
+  },
+  {
+    path: "IntroToPhotography",
+    title: "مقدمة في التصوير",
+  },
+  {
+    path: "Composition",
+    title: "تكوين الصورة",
+  },
+  {
+    path: "LightProperties",
+    title: "خصائص الضوء و أنواعه",
+  },
+  {
+    path: "ExposureTriangle",
+    title: "مثلث التعريض الضوئي",
+  },
+  {
+    path: "WhiteBalance",
+    title: "موازنة اللون الأبيض",
+  },
+  {
+    path: "LightMetering",
+    title: "مقياس الضوء",
+  },
+  {
+    path: "CameraFocus",
+    title: "التركيز و خصائصه",
+  },
+  {
+    path: "CameraFlash",
+    title: "فلاش الكاميرا",
+  },
+  {
+    path: "CameraLenses",
+    title: "عدسات الكاميرا",
+  },
+  {
+    path: "LightRoom",
+    title: "معالجة الصور ببرنامج لايت روم",
+  },
+  {
+    path: "PhotographySpecializations",
+    title: "محاور التصوير الفوتوغرافي",
+  },
+  {
+    path: "LandscapePhotography",
+    title: "تصوير الطبيعة",
+  },
+  {
+    path: "NightPhotography",
+    title: "التصوير الليلي",
+  },
+  {
+    path: "ArchitecturePhotography",
+    title: "التصوير المعماري",
+  },
+  {
+    path: "PortraitPhotography",
+    title: "تصوير البورتريت",
+  },
+  {
+    path: "PhotoJournalist",
+    title: "التصوير الصحفي",
+  },
+  {
+    path: "SportsPhotography",
+    title: "تصوير الرياضة",
+  },
+  {
+    path: "AnimalsPhotography",
+    title: "تصوير الحيوانات",
+  },
+  {
+    path: "FlowersPhotography",
+    title: "تصوير الزهور",
+  },
+  {
+    path: "FinalWords",
+    title: "كلمة أخيرة",
+  },
+];
