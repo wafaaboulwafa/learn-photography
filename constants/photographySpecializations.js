9523c647e16e02e64997b0fae26a76d79d39ccce
Wafa Aboulwafa <67149147+wa250031@users.noreply.github.com>
2024-10-11 10:49:13 +0300

    spec

--- constants/photographySpecializations.js
+++ constants/photographySpecializations.js
@@ -1,18 +1,90 @@
 export default {
   id: 3,
-  title: "",
+  title: "محاور التصوير الفوتوغرافي",
   paragraphs: [
     {
       type: "header",
-      value: "",
+      value: "محاور التصوير الفوتوغرافي",
     },
     {
       type: "paragraph",
-      value: "",
+      value:
+        "إن التصوير الفوتوغرافي كما باقي المهن فيه الكثير من الاختصاصات و التي تسمى بمحاور التصوير. ولا ينبغي عليك إجادة جميع المحاور و إنما التركيز على إتقان محور معين حتى تستطيع الإبداع به.",
     },
     {
-      type: "image",
-      value: null,
+      type: "paragraph",
+      value:
+        "إن اختيارك للمحور الذي ستعمل عليه ربما حسب ميلوك الشخصي أو طبيعة عملك.",
+    },
+    {
+      type: "paragraph",
+      value:
+        "هذا الكتاب يقدم لك اساسيات التصوير لكيفية استعمال الكاميرا بشكل عام و تجنب و حل المشاكل أثناء التصوير بغض النظر عن المحور المستعمل.  إلا أننا سنقدم مدخلاً لبعض المحاور الشائعة.",
+    },
+    {
+      type: "paragraph",
+      value:
+        "قبل ذلك ربما من المفيد ذكر بعض المحاور الشائعة الموجودة في التصوير.",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير الطبيعة  Landscape Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	التصوير الليلي  Night Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	التصوير المعماري Architecture Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	البورتريت أو تصوير الاشخاص Portrait Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير الموضة Fashion Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير الأفراح  Wedding Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير حياة الشارع  Street Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	التصوير الصحفي Photo Journalist",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير المنتجات Products Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير الماكرو Macro Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير الرياضة Sports Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	الحياة الصامتة Still life Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	تصوير الحيوانات Animals Photography",
+    },
+    {
+      type: "paragraph",
+      value: "•	الفاين أرت Fine Art",
+    },
+    {
+      type: "paragraph",
+      value: "•	التصوير التجريدي Abstract Photography",
     },
   ],
 };
